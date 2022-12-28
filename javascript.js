d038a67dd49817ca1ecc05d14c9c13d87c8e1696
raybarkerrr <raybarkerrr@gmail.com>
2022-12-27 20:07:37 -0600

    New Better working navbar

--- javascript.js
+++ javascript.js
@@ -1,8 +1,3 @@
-// nav toggle - select button and links
-const navToggle = document.querySelector("#navToggle")
-const nav = document.querySelector("#nav-links")
-
-// add event listener
-navToggle.addEventListener("click", () => {
-    nav.classList.toggle('nav-open')
-})
+function toggleMobileMenu(menu) {
+    menu.classList.toggle('open');
+}
